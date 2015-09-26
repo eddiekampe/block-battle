b5d554ee887b609a8f160d032549c41e5cd38ce7
Eddie Kämpe <eddiek@kth.se>
2015-09-26 23:08:30 +0200

    Output should always have new line and be flushed

--- game/helper/output.py
+++ game/helper/output.py
@@ -1,13 +1,20 @@
+import os
 import sys
 
 
-class Response(object):
+class Output(object):
 
     @staticmethod
-    def put(string):
+    def write(string):
         """
         Helper method to make sure new line and flush is not omitted
         :param string: String to write
         """
         sys.stdout.write(string + "\n")
         sys.stdout.flush()
+
+    @staticmethod
+    def debug(string):
+
+        if os.getenv("DEBUG", default=False):
+            print string
